71f94652f10c483bc3cfee78b538352c50d54b99
Joanna C <joannachowhy@gmail.com>
2023-03-29 14:55:38 -0400

    set routes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Chow_J_AVResearch
- 
+# FIP Team Page
+This is a team page that showcase each team member and their role for the FIP project in winter 2023
+
+## Contributors/Teams
+### Graphic Design - Gonzalo Martin
+### Video Design - Abram Kouwen
+### Marketing/Campaign - Hannah Di Renzo
+### Full Stack Development - Hei Yan Chow
+### Project Management - Zeyin Lu
+
+## Built With
+    - Node
+    - Express
+
+## License
+MIT
+
--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-const express = require('express'); // import the express package
-const app = express(); // create an express app
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = process.env.PORT || 3001;
+var indexRouter = require('./routes/index');
 
-app.use(express.static('public'));
+var app = express();
 
-// this is a route handler -> listen for incoming requests and send back a response
-// this is the root route -> expects to render an index page
-app.get('/', (req, res) => {
-    // this is pointing at index.html -> shared team page
-  res.sendFile(__dirname + '/index.html');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/video', (req, res) => {
-    // this is pointing at index.html -> shared team page
-  res.sendFile(__dirname + '/video.html');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get('/audio', (req, res) => {
-  // this is pointing at index.html -> shared team page
-res.sendFile(__dirname + '/audio.html');
-});
-
-// set up the server to listen for incoming connections at this port
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+module.exports = app;
